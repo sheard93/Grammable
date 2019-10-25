26d99e0cb13e50c9f712b6f031c8614a320daeb8
sheard93 <shelbyzaglin@gmail.com>
2019-10-25 18:40:20 +0000

    Add a gram to the database

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,23 @@
 class GramsController < ApplicationController
+  def new
+    @gram = Gram.new
+  end
+
   def index
   end
+
+  def create
+    @gram = Gram.create(gram_params)
+    redirect_to root_path
+  end
+
+
+  private
+
+  def grams_params
+    params.require(:gram).permit(:message)
+  end
+
+
+
 end
